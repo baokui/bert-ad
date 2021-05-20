47eb83bbb31f167f75dd772997850d544bb9e7d7
apple-guobk <baokui2011@163.COM>
2021-05-20 12:57:29 +0800

    test

--- bertSentvec.py
+++ bertSentvec.py
@@ -28,7 +28,7 @@ import tensorflow as tf
 import json
 import numpy as np
 import sys
-from run_classifier_mgpu import average_gradients
+
 flags = tf.flags
 
 FLAGS = flags.FLAGS
@@ -154,6 +154,62 @@ class InputExample(object):
         self.text_b = text_b
         self.label = label
 
+def average_gradients(tower_grads, batch_size, options):
+  # calculate average gradient for each shared variable across all GPUs
+  average_grads = []
+  count = 0
+  for grad_and_vars in zip(*tower_grads):
+    # Note that each grad_and_vars looks like the following:
+    #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+    # We need to average the gradients across each GPU.
+    count += 1
+    g0, v0 = grad_and_vars[0]
+
+    if g0 is None:
+      # no gradient for this variable, skip it
+      average_grads.append((g0, v0))
+      continue
+
+    if isinstance(g0, tf.IndexedSlices):
+      # If the gradient is type IndexedSlices then this is a sparse
+      #   gradient with attributes indices and values.
+      # To average, need to concat them individually then create
+      #   a new IndexedSlices object.
+      indices = []
+      values = []
+      for g, v in grad_and_vars:
+        indices.append(g.indices)
+        values.append(g.values)
+      all_indices = tf.concat(indices, 0)
+      avg_values = tf.concat(values, 0) / len(grad_and_vars)
+      # deduplicate across indices
+      av, ai = _deduplicate_indexed_slices(avg_values, all_indices)
+      grad = tf.IndexedSlices(av, ai, dense_shape=g0.dense_shape)
+
+    else:
+      # a normal tensor can just do a simple average
+      grads = []
+      for g, v in grad_and_vars:
+        # Add 0 dimension to the gradients to represent the tower.
+        expanded_g = tf.expand_dims(g, 0)
+        # Append on a 'tower' dimension which we will average over
+        grads.append(expanded_g)
+
+      # Average over the 'tower' dimension.
+      grad = tf.concat(grads, 0)
+      grad = tf.reduce_mean(grad, 0)
+
+    # the Variables are redundant because they are shared
+    # across towers. So.. just return the first tower's pointer to
+    # the Variable.
+    v = grad_and_vars[0][1]
+    grad_and_var = (grad, v)
+
+    average_grads.append(grad_and_var)
+
+  assert len(average_grads) == len(list(zip(*tower_grads)))
+
+  return average_grads
 
 class PaddingInputExample(object):
     """Fake example so the num input examples is a multiple of the batch size.
